9020978261a7414ddf5b02131f02044df51fc738
dperformance <docyoungs@naver.com>
2021-03-25 08:57:51 +0900

    학생 도메인 정보를 추가하라

--- app/src/main/java/com/dyson/school/App.java
+++ app/src/main/java/com/dyson/school/App.java
@@ -9,12 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Worldzz!";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-
-        System.out.println(new App().getGreeting());
     }
 }
---
+++ app/src/main/java/com/dyson/school/domain/Student.java
@@ -0,0 +1,34 @@
+// TODO
+// id
+// name
+// password
+// 학년반 - group
+// 생년월일 - birthday
+// 성별 - gender
+// 핸드폰번호 - phone
+// 주소 - address
+// image
+
+package com.dyson.school.domain;
+
+public class Student {
+
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String group;
+
+    private String birthday;
+
+    private String gender;
+
+    private String phone;
+
+    private String address;
+
+    private String image;
+
+}
